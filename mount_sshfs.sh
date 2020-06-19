c261d75cb9c1a608cf3b17e7d8e108847e953b26
Pavel Ershov <pershov@game-insight.com>
2020-06-19 15:07:08 +0300

    Some commentaries

---
+++ mount_sshfs.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+
+# MacMini
+# ssh devnul@192.168.1.24
+# mkdir Projects_macmini
+# sshfs devnul@192.168.1.13:/Users/devnul/Projects_macmini ./Projects_macmini
+# umount Projects_macmini
